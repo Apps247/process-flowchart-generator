344a24799bd1fcba6d76a81687e4b33bdd2bf3ec
Apps247 <67941617+Apps247@users.noreply.github.com>
2024-05-29 11:56:50 +0000

    Updated Decision logic

--- scripts/flowchart_node.py
+++ scripts/flowchart_node.py
@@ -1,8 +1,34 @@
 class FlowchartNode:
-    def __init__(self, id, visioId, type, title, description, actor):
+    def __init__(self, id, visioId, type, title, description, actor=None, decisions=None):
         self.id = id
         self.visioId = visioId
         self.type = type
         self.title = title
         self.description = description
         self.actor = actor
+        self.decisions = decisions
+
+    @staticmethod
+    def from_spreadsheet_row(row):
+        id = str(row["Step ID"]).strip()
+        description = str(row["Description"]).strip()
+        actor = str(row["Responsible"]).strip()
+        decisions = [s.strip() for s in str(row["Decision"]).strip().split("/")]
+        if '' in decisions:
+            decisions.remove('')
+        if 'nan' in decisions:
+            decisions.remove('nan')
+
+        print(decisions)
+        try:
+            if len(decisions) > 0:
+                type = "Decision"
+            else:
+                type = "Process"
+        except AttributeError:
+            pass
+
+        return FlowchartNode(id, None, type, None, description, actor, decisions)
+
+    def is_decision(self):
+        return self.type == "Decision"
--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,7 @@
 # Main script to automate the process of generating Visio flowcharts from notes on a spreadsheet
 # TODO: Add Aspose License details
+# ? Aspose Docs: https://docs.aspose.com/diagram/java/working-with-visio-shape-data/#use-connection-indexes-to-connect-shapes
+
 
 import jpype
 import pandas as pd
@@ -16,6 +18,9 @@ OUTPUT_DIRECTORY = "../output"
 INPUT_DIRECTORY = "../input"
 TEMPLATE_DIRECTORY = "../templates"
 
+PAGE_SPACING_X_FACTOR = 2.5
+PAGE_SPACING_Y_FACTOR = 2.5
+
 # Read meeting notes spreadsheet file from input directory
 input_file = f"{INPUT_DIRECTORY}/Meeting_notes.xlsx"
 df = pd.read_excel(input_file)
@@ -24,41 +29,80 @@ df = pd.read_excel(input_file)
 template_file = f"{TEMPLATE_DIRECTORY}/Basic Shapes.vss"
 diagram = Diagram(template_file)
 
-# Parse notes content to extract flowchart elements
 flowchart_nodes = []
-
-print(diagram.getMasters().isExist("Rectangle"))
-print(diagram.getMasters().isExist("Process"))
-print(diagram.getMasters().isExist("Diamond"))
-print(diagram.getMasters().isExist("Circle"))
-print(df)
-
 page = diagram.getPages().getPage("Page-1")
 
-for index, row in df.iterrows():
-    # Extract flowchart elements from the row
-    print(row)
-    id = row[0]
-    description = row["Description"]
-    actor = row["Responsible"]
-    try:
-        type = "Decision" if len(row["Decision"].strip()) else "Process"
-    except AttributeError:
-        pass
 
-    # Create a FlowchartNode object
-    flowchart_node = FlowchartNode(id, None, type, None, description, actor)
-    flowchart_nodes.append(flowchart_node)
+def render_flowchart():
+    index = 0
+    offset = 0
+    nest_level = 1
+    max_nest_level = 1
 
-    # Add the FlowchartNode object to the diagram
-    flowchart_node.visioId = diagram.addShape(
-        4.5 * len(flowchart_nodes), 5.5, 2, 1, "Rectangle", 0)
-    # flowchart_node.visioId = diagram.addShape(
-    #     5 * len(flowchart_nodes), 5, 10, 10, flowchart_node.type, 0)
+    in_alternate_path = False
+
+    while index < len(df):
+        row = df.iloc[index]
+        print(index)
+        # Extract flowchart elements from the row into FlowchartNode object
+        flowchart_node = FlowchartNode.from_spreadsheet_row(row)
+
+        print(flowchart_node.description)
+
+        # if flowchart_node.description.split(":")[-1].strip().split(' ')[0] == "Proceed":
+        if flowchart_node.description.split(":")[-1].strip().split(' ')[0] == "Proceed":
+            nest_level -= 1
+            index += 1
+            print("Proceed-popping")
+            print("Proceed to", flowchart_node.description.split(
+                ":")[-1].strip().split(' ')[-1])
+            offset -= 1
+            continue
+
+        flowchart_nodes.append(flowchart_node)
+
+        # Add the FlowchartNode object to the diagram
+
+        flowchart_node.visioId = diagram.addShape(
+            PAGE_SPACING_X_FACTOR * (index + offset),
+            PAGE_SPACING_Y_FACTOR * nest_level, 2, 1, "Rectangle", 0)
+
+        render_text(flowchart_node)
+
+        print(flowchart_node.type)
+        try:
+            if flowchart_node.is_decision():
+                print(flowchart_node.decisions)
+                nest_level += 1
+                max_nest_level += 1
+                index += 2
+                offset -= 2
+            else:
+                index += 1
+        except AttributeError as e:
+            print("Attribute Error:", e)
+            index += 1
+
+        print("End", index)
+
+    page.getPageSheet().getPageProps().getPageHeight().setValue(
+        PAGE_SPACING_Y_FACTOR * max_nest_level)
+    page.getPageSheet().getPageProps().getPageWidth().setValue(
+        PAGE_SPACING_X_FACTOR * (len(flowchart_nodes) + offset))
+
+    # Save Visio file
+    diagram.save(f"{OUTPUT_DIRECTORY}/output.vsdx", SaveFileFormat.VSDX)
+
+
+def render_text(flowchart_node):
+    # Render FlowchartNode text on page
     shape = page.getShapes().getShape(flowchart_node.visioId)
-    shape.getText().getValue().add(Txt(f"\n{flowchart_node.description}\n({flowchart_node.actor})"))
+    text = f"{flowchart_node.description}\n({flowchart_node.actor})".replace(
+        "(nan)", "")
+    shape.getText().getValue().add(Txt(text))
 
 
-# Save Visio file
-diagram.save(f"{OUTPUT_DIRECTORY}/output.vsdx", SaveFileFormat.VSDX)
+if __name__ == "__main__":
+    render_flowchart()
+
 jpype.shutdownJVM()
